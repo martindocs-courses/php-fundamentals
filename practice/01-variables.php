63beee32b2fdfb4126c34ae1db64b58b62371f84
Marcin Tatarski <martin.tatarski@gmail.com>
2024-03-12 23:34:30 +0000

    Add variable practice challenge1

---
+++ practice/01-variables.php
@@ -0,0 +1,27 @@
+<?php
+$h1 = 'Basic of PHP';
+$pageTitle = 'Martin Practice 1';
+$author = 'Martin';
+$htmlBody = '
+  PHP (Hypertext Preprocessor) is a widely used server-side     scripting language that has revolutionized web development. With its simplicity, flexibility, and vast community support, PHP has become the backbone of countless dynamic websites and web applications.
+  '
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php echo "$pageTitle | Variables" ?></title>
+</head>
+
+<body>
+  <h1><?= $h1 ?></h1>
+  <p>by: <?= $author ?></p>
+  <p>
+    <?= $htmlBody ?>
+  </p>
+</body>
+
+</html>
